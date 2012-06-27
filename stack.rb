8046dd6c2f585ba6890b5c7a70068a2fa27bca69
Shigenobu Nishikawa <snishikawa@aiming-inc.com>
2012-06-27 20:34:53 +0900

    add size method

--- stack.rb
+++ stack.rb
@@ -8,15 +8,38 @@ class Stack
   def empty?
     @elements.empty?
   end
+
+  def size
+    @elements.size
+  end
 end
 
 describe Stack do 
-  context 'empty' do
-    its(:empty?){ should be_true }
+  describe '#empty?' do
+    context 'empty' do
+      its(:empty?){ should be_true }
+    end
+    context 'not empty' do
+      let(:stack){ Stack.new(1) }
+      subject { stack }
+      its(:empty?){ should be_false }
+    end
   end
-  context 'not empty' do
-    let(:stack){ Stack.new(1) }
-    subject { stack }
-    its(:empty?){ should be_false }
+  describe '#size' do
+    context 'when stack is empty' do
+      its(:size) { should == 0 }
+    end
+    context 'when stack is not empty' do
+      context 'stack size is 1' do
+        let(:stack){ Stack.new(1) }
+        subject { stack }
+        its(:size){ should == 1 }
+      end
+      context 'stack size is 2' do
+        let(:stack){ Stack.new(1, 2) }
+        subject { stack }
+        its(:size){ should == 2 }
+      end 
+    end
   end
 end
